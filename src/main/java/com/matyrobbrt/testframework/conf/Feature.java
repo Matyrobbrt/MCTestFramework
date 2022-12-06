e2e4833f2f001e565b85dbaa284bdc97c0c4e204
Matyrobbrt <matyrobbrt@gmail.com>
2022-12-06 18:45:14 +0200

    Document features

--- src/main/java/com/matyrobbrt/testframework/conf/Feature.java
+++ src/main/java/com/matyrobbrt/testframework/conf/Feature.java
@@ -1,9 +1,30 @@
 package com.matyrobbrt.testframework.conf;
 
+/**
+ * This class represents the different features a {@link com.matyrobbrt.testframework.TestFramework} may have.
+ *
+ * @see FrameworkConfiguration.Builder#enable(Feature...)
+ * @see FrameworkConfiguration.Builder#disable(Feature...)
+ */
 public enum Feature {
+    /**
+     * This feature syncs the status of tests to clients.
+     */
     CLIENT_SYNC(false),
+
+    /**
+     * This feature allows clients to modify the status of tests.
+     */
     CLIENT_MODIFICATIONS(false),
+
+    /**
+     * This feature enables the GameTest integration.
+     */
     GAMETEST(true),
+
+    /**
+     * When enabled, test summaries will be dumped when a server stops.
+     */
     SUMMARY_DUMP(true),
 
     /**
